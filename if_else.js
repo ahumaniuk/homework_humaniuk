1b8f40fb2695941ebf33be623adc3b961e1716bb
ahumaniuk <a.humaniuk@gmail.com>
2023-08-31 13:47:05 +0300

    added_else_clauss

--- if_else.js
+++ if_else.js
@@ -1,4 +1,15 @@
 let num215 = 215;
 if (num215 ==215) {
   console.log('yes value is under condition')
+  console.log('1')
+}
+
+if (num215 >= 215 && num215 <=250){
+  console.log('second condition works')
+}
+
+if (num215 >500){
+  console.log('num215 is greater thatn 500')
+} else {
+  console.log('code goes to the else')
 }
