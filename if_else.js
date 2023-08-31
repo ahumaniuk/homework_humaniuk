e28212065f6c5d8ddada98ceff6bd3bbb00433cc
ahumaniuk <a.humaniuk@gmail.com>
2023-08-31 17:42:52 +0300

    updated

--- if_else.js
+++ if_else.js
@@ -13,3 +13,15 @@ if (num215 >500){
 } else {
   console.log('code goes to the else')
 }
+
+if (num215 >500){
+  console.log('num215 is greater thatn 500')
+} else if (num215 > 250 ) {
+  console.log('num greater than 250')
+} else if (num215 >= 200 ){
+  console.log ('num215 = 200')
+} else if (Number.isInteger(num215) ){
+  console.log ('num is integer')
+}else {
+  console.log('code goes to the else')
+}
